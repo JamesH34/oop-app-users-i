f714013140209606b62fb1631df5860974f3ec0c
James Hershey <jameshershey34@gmail.com>
2024-02-07 15:57:38 -0500

    user info gathered

--- User.py
+++ User.py
@@ -1,15 +1,23 @@
 class User:
 
-    def __init__(self, name, email, username, password):
+    def __init__(self, name, email, username, drivers_license):
         self.name = name 
         self.email = email
         self.username = username
-        self.password = password
+        self.drivers_license = drivers_license
 
     def __str__(self):
-        return f"I am {self.name} my email is {self.email} my username is {self.username} and password is {self.password}."
+        return f"I am {self.name} my email is {self.email} my username is {self.username} and drivers_license is {self.drivers_license}."
 
-john = User("John", "john@gmail.com", "John123", "John's-password")
-kevin = User("kevin", "kevin@gmail.com", "kevin123", "kevin's-password")
+    def password(self, secret_code):
+        print(f"{self.name}'s password is {secret_code}")
+
+
+
+
+john = User("John", "john@gmail.com", "John123", "123456")
+kevin = User("kevin", "kevin@gmail.com", "kevin123", "1098765")
+kevin.password("12345")
+john.password("password789")
 print(john)
 print(kevin)
